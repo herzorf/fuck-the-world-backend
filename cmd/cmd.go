a5a4337237bfca4bfe29dd7a32913bfa82fe9a69
herzorf <1446450047@qq.com>
2025-02-18 21:37:06 +0800

    feat: 连接数据库

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"bookkeeping-server/database"
 	"bookkeeping-server/internal/router"
 )
 
 func RunServer() {
+	database.Connect()
 	r := router.New()
 	err := r.Run() // listen and serve on 0.0.0.0:8080
 	if err != nil {
