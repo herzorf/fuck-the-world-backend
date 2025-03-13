4ae0434837fc28e3c75073f04d98f351bbf76c8f
herzorf <1446450047@qq.com>
2025-03-13 21:25:18 +0800

    feat: 创建validation_codes 并做数据迁移

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -23,9 +23,17 @@ func Run() {
 	var dbCmd = &cobra.Command{
 		Use: "db",
 	}
-
+	migrateCmd := &cobra.Command{
+		Use:   "migrate",
+		Short: "迁移数据库",
+		Long:  `迁移数据库`,
+		Run: func(cmd *cobra.Command, args []string) {
+			database.Migrate()
+		},
+	}
 	rootCmd.AddCommand(serverCmd)
 	rootCmd.AddCommand(dbCmd)
+	dbCmd.AddCommand(migrateCmd)
 	database.Connect()
 	err := rootCmd.Execute()
 	if err != nil {
--- database/database.go
+++ database/database.go
@@ -6,7 +6,17 @@ import (
 	_ "github.com/lib/pq"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
+
+type ValidationCode struct {
+	ID        uint       `gorm:"primaryKey"`
+	Code      string     `gorm:"size:20;not null"`
+	Email     string     `gorm:"size:255;not null"`
+	UsedAt    *time.Time // 可以为空，代表未使用
+	CreatedAt time.Time  `gorm:"autoCreateTime"`
+	UpdatedAt time.Time  `gorm:"autoUpdateTime"`
+}
 
 var (
 	host     = "localhost"
@@ -23,5 +33,10 @@ func Connect() {
 	unit.HandleError("数据库连接失败", err)
 	DB = dbRoot
 	// 如果数据库不存在就创建数据库
-	dbRoot.Exec("CREATE DATABASE IF NOT EXISTS " + dbname + " CHARSET utf8mb4 COLLATE utf8mb4_general_ci;")
+	DB.Exec("CREATE DATABASE IF NOT EXISTS " + dbname + " CHARSET utf8mb4 COLLATE utf8mb4_general_ci;")
+	DB.Exec(fmt.Sprintf("USE %s", dbname))
+}
+
+func Migrate() {
+	DB.AutoMigrate(&ValidationCode{})
 }
