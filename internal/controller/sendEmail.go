77e1b72be73433e001006a4cbe5fe5d8c2284b9b
herzorf <1446450047@qq.com>
2025-03-16 21:27:54 +0800

    feat: 添加错误处理

--- internal/controller/sendEmail.go
+++ internal/controller/sendEmail.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"bookkeeping-server/unit"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -12,7 +13,10 @@ func SendEmail(c *gin.Context) {
 		Email string `json:"email"`
 	}
 	var aimEmail = &Email{}
-	c.ShouldBindJSON(aimEmail)
+	err := c.ShouldBindJSON(aimEmail)
+	if err != nil {
+		unit.HandleError("sendEmail接口数据读取失败", err)
+	}
 	log.Println("发送邮件给", aimEmail)
 	c.String(http.StatusBadRequest, fmt.Sprintf("发送邮件给:%s", aimEmail.Email))
 }
