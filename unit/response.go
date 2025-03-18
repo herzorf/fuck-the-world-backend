e9632388ff6e30e7376c8fc98f572fc5a578bfb1
herzorf <1446450047@qq.com>
2025-03-18 21:11:35 +0800

    feat: 封装一个响应函数

---
+++ unit/response.go
@@ -0,0 +1,11 @@
+package unit
+
+import "github.com/gin-gonic/gin"
+
+func RespondJSON(c *gin.Context, code int, message string, result interface{}) {
+	c.JSON(code, gin.H{
+		"code":    code,
+		"message": message,
+		"result":  result,
+	})
+}
