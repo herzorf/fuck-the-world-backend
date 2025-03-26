912605269f595b1813ea76f2b92f84774f31de9b
herzorf <1446450047@qq.com>
2025-03-26 14:21:36 +0800

    feat: login接口返回用户名和id

--- internal/controller/login.go
+++ internal/controller/login.go
@@ -43,7 +43,9 @@ func Login(c *gin.Context) {
 			unit.HandleError("生成JWT失败", err)
 		}
 		unit.RespondJSON(c, http.StatusOK, "登录成功", gin.H{
-			"jwt": jwt,
+			"jwt":      jwt,
+			"username": user.Username,
+			"id":       user.ID,
 		})
 		return
 	}
