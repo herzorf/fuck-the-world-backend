ccb535b820628cb9cdce424d553be8434a453096
herzorf <1446450047@qq.com>
2025-03-27 16:22:32 +0800

    feat: user删除email字段

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,7 +6,6 @@ type User struct {
 	ID        uint   `gorm:"primaryKey"`
 	Username  string `gorm:"size:255;not null"`
 	Password  string `gorm:"size:255;not null"`
-	Email     string `gorm:"size:255;not null"`
 	Role      string `gorm:"size:50;not null;default:operator"` // 角色字段
 	CreatedAt time.Time
 	UpdatedAt time.Time
