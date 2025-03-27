692328658d90dce0980fd7cfeac387097f048cce
herzorf <1446450047@qq.com>
2025-03-27 21:24:26 +0800

    feat: 重构路由

--- internal/router/router.go
+++ internal/router/router.go
@@ -26,7 +26,7 @@ import (
 func New() *gin.Engine {
 	config.LoadConfigYaml()
 	r := gin.New()
-	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.BasePath = "/api"
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	r.Use(cors.New(cors.Config{
@@ -35,16 +35,21 @@ func New() *gin.Engine {
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		AllowCredentials: true, // 允许携带 Cookie
 	}))
-	r.POST("/api/v1/login", controller.Login)
+	r.POST("/api/login", controller.Login)
+	//需要登陆的接口
+	authGroup := r.Group("/api")
 	{
-		//需要登陆的接口
-		authGroup := r.Group("/api/v1")
-		authGroup.Use(middleware.AuthMiddleware(), middleware.AuthAdminMiddleWare())
-		authGroup.POST("/createOperator", controller.CreateOperator)
+		authGroup.Use(middleware.AuthMiddleware())
 		authGroup.POST("/ping", controller.Ping)
 		authGroup.POST("/sendEmail", controller.SendEmail)
 	}
-
+	//需要登录和管理员权限的接口
+	authAdminGroup := r.Group("/api")
+	{
+		authAdminGroup.Use(middleware.AuthMiddleware(), middleware.AuthAdminMiddleWare())
+		operateGroup := authAdminGroup.Group("/operator")
+		SetupUserRoutes(operateGroup)
+	}
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
