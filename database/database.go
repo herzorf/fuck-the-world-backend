2e6533767dd6cddb94ceb64896a94feb66dc4453
herzorf <1446450047@qq.com>
2025-04-03 14:10:59 +0800

    feat: 修复多次重新连接数据库包错

--- database/database.go
+++ database/database.go
@@ -20,19 +20,26 @@ func Connect() {
 	dbname := viper.GetString("database.dbname")
 	dsnRoot := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbname)
 	log.Println(dsnRoot)
-	var err error
+	var error error
 	for i := 0; i < 5; i++ { // 最多尝试 5 次
-		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		db, err := gorm.Open(mysql.Open(dsnRoot), &gorm.Config{})
 		if err == nil {
+			DB = db
+			log.Println("数据库连接成功")
 			break // 连接成功
 		}
-		log.Println(i+1,"数据库连接失败, 重试中...", err)
+		error = err
+		log.Println(i+1, "数据库连接失败, 重试中...", error)
 		time.Sleep(5 * time.Second) // 等待 5 秒再试
 	}
-	unit.HandleError("数据库连接失败", err)
-	DB = db
-	sqlDB, err := db.DB()
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(time.Minute * 30)
+	unit.HandleError("数据库连接5次失败", error)
+	sqlDB, err := DB.DB()
+	if err != nil {
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetMaxOpenConns(100)
+		sqlDB.SetConnMaxLifetime(time.Minute * 30)
+	} else {
+		unit.HandleError("获取数据库连接池失败", err)
+	}
+
 }
