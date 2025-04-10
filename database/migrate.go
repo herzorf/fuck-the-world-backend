8c7e3cf2f4d4b5c567987c9d2ff18e36e2964af2
herzorf <1446450047@qq.com>
2025-04-10 15:03:38 +0800

    feat: 地图表迁移

--- database/migrate.go
+++ database/migrate.go
@@ -2,13 +2,16 @@ package database
 
 import (
 	"fuck-the-world/internal/model"
-	"fuck-the-world/unit"
+	"fuck-the-world/utils"
 )
 
 func Migrate() {
 	err := DB.AutoMigrate(&model.ValidationEmailCode{})
 	err = DB.AutoMigrate(&model.User{})
+	err = DB.AutoMigrate(&model.Map{})
+	err = DB.AutoMigrate(&model.Player{})
+	err = DB.AutoMigrate(&model.MapPlayerSave{})
 	if err != nil {
-		unit.HandleError("数据库迁移失败", err)
+		utils.HandleError("数据库迁移失败", err)
 	}
 }
